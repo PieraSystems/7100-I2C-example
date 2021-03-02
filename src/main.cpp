07cc7f0866e1a093a2a9f9f967a4591efa24fa36
Will Harader <wharader@gmail.com>
2021-03-02 10:24:18 -0800

    tidying code, more comments in example

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,34 @@
 
 #include <IpsI2C.h>
 
-byte rcvData = 0;
-byte rcvData2 = 0;
-
 IpsSensor ips_sensor;
 
 void setup()
 {
   delay(3500);
+  // Initiate I2C connection SDA 21, SCL 22 by default
+  // Set different pins by using:
+  // ips_sensor.begin(SDA_PIN, SCL_PIN)
   ips_sensor.begin();
+
+  // Initiate USB serial at 115200 baud
   Serial.begin(115200);
   Serial.println("\nI2C Test");
 }
 
 void loop()
 {
+  // Get new IPS sensor readings
+  // Not meant to run more than once per second
   ips_sensor.update();
+
+  // Print PC0.1 via USB serial
   Serial.println(ips_sensor.getPC01());
+
+  // Print PM1.0 via USB serial
   Serial.println(ips_sensor.getPM10());
+
+  // Print PM2.5 via USB serial
   Serial.println(ips_sensor.getPM25());
 
   delay(1000);
